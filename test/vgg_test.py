783ece805248c6866691d3e0f0f04337e726b428
xfj <372994102@qq.com>
2018-09-10 20:50:19 +0800

    update

--- test/vgg_test.py
+++ test/vgg_test.py
@@ -2,6 +2,10 @@ from keras.applications import VGG16
 import os
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
+from keras import models
+from keras import layers
+from keras import optimizers
+import matplotlib.pyplot as plt
 
 conv_base = VGG16(include_top=False, input_shape=(150, 150, 3))
 conv_base.summary()
@@ -29,9 +33,39 @@ def extract_features(directory, sample_count):
     return features, labels
 
 
+# 从vgg16预测4*4*512特征图
 train_features, train_labels = extract_features(train_dir, 2000)
 validation_features, validation_labels = extract_features(validation_dir, 1000)
 test_features, test_labels = extra_features(test_dir, 1000)
 train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
 validation_features = np.reshape(validation_features, (1000, 4 * 4 * 512))
 test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+# 定义分类器使用vgg16预测的结果
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
+# 二分类问题采用sigmoid损失函数
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizers=optimizers.RMSprop(lr=2e-5), loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(train_features, train_labels, epochs=30, batch_size=20,
+                    validation_data=(validation_features, validation_labels))
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, acc, 'b', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'y', label='Training loss')
+plt.plot(epochs, val_loss, 'w', label='Validation loss')
+plt.title('Loss')
+plt.legend()
+plt.figure()
+
+plt.show()
+
