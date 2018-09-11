6b9ca5136ed510044598183de8a16c0ed320b5d3
xiaoai119 <372994102@qq.com>
2018-09-12 03:22:19 +0800

    modify

--- test/vgg_freeze.py
+++ test/vgg_freeze.py
@@ -24,14 +24,15 @@ model.summary()
 
 train_datagen = ImageDataGenerator(rescale=1. / 255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2,
                                    shear_range=0.2, horizontal_flip=True, fill_mode='nearest')
+# train_datagen = ImageDataGenerator(rescale=1. / 255)
 test_datagen = ImageDataGenerator(rescale=1. / 255)
 train_generator = train_datagen.flow_from_directory(train_dir, target_size=(150, 150), batch_size=100,
                                                     class_mode='binary')
 validation_generator = test_datagen.flow_from_directory(test_dir, target_size=(150, 150), batch_size=100,
                                                         class_mode='binary')
 model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=2e-5), metrics=['acc'])
-history = model.fit(train_generator, epochs=300, batch_size=100,
-                    validation_data=validation_generator, validation_steps=100)
+history = model.fit_generator(train_generator, epochs=300, steps_per_epoch=100,
+                    validation_data=validation_generator,validation_steps=100)
 
 
 def draw_result():
